21834e3b10b6cee93ce43cc250e3781c4bd59a62
Takashi SASAKI <takashi316@gmail.com>
2018-11-28 22:28:28 +0900

    hello sqlite3

--- sethash.py
+++ sethash.py
@@ -1,5 +1,11 @@
 import xor
+import sqlite3
 
 if __name__ == "__main__":
-    print(xor.xor("abc","abc"))
-    pass
+    print(xor.xorUtf8Sha1All(["hello", "world"]))
+
+    connection = sqlite3.connect("test.sqlite3")
+    cursor = connection.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS test (col1, col2, col3)")
+    connection.commit()
