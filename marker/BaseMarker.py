2ada1a9197407a708eaf13d48a837eeaa18c96e4
Takashi SASAKI <takashi316@gmail.com>
2018-12-05 08:03:19 +0900

    marker module

---
+++ marker/BaseMarker.py
@@ -0,0 +1,2 @@
+class BaseMarker(object):
+    pass
---
+++ marker/FileMarker.py
@@ -0,0 +1,19 @@
+import marker.BaseMarker
+import os
+import os.path
+
+class FileMarker(marker.BaseMarker.BaseMarker):
+    def __init__(self, filePath):
+        self.writable = False
+        if not os.path.isabs(filePath):
+            raise RuntimeError(filePath + " is not absolute path")
+        if os.path.isdir(filePath):
+            self.directory = filePath
+            self.file = None
+            return
+        if os.path.isfile(filePath):
+            self.file = filePath
+            head, tail = os.path.split(filePath)
+            parent = os.path.join(head)
+
+    
