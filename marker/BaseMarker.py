e1c0f570f70687745433ddf6ec488da99ac9c641
Takashi SASAKI <takashi316@gmail.com>
2018-12-05 08:37:45 +0900

    move checkDnsMagic to BaseMaker.py

--- marker/BaseMarker.py
+++ marker/BaseMarker.py
@@ -1,2 +1,23 @@
+import dns.resolver
+import logging
+
 class BaseMarker(object):
+    magic = b"c39bf83a4d185b0c2a1c2d0a069d37ae6c0cbc44"
     pass
+
+def getDnsTxts(fqdn):
+    answer = dns.resolver.query(fqdn, "TXT")
+    answer0 = answer.response.answer[0]
+    item = answer0.items[0]
+    strings = item.strings
+    return strings
+
+def checkDnsMagic():
+    strings = getDnsTxts("magic.moukaeritai.work")
+    if len(strings) != 1:
+         logging.warning("Failed to get TXT record of magic.moukaeritai.work.")
+    if strings[0] != BaseMarker.magic:
+         logging.warning("Magic number does not match with magic.moukaeritai.work.")
+
+if __name__ == "__main__":
+    checkDnsMagic()    
--- marker/Marker.py
+++ marker/Marker.py
@@ -5,17 +5,8 @@ import logging
 print(urllib.parse.urlsplit("file://surfacepro5/c:/a/b/c"))
 
 class Marker():
-    magic = "c39bf83a4d185b0c2a1c2d0a069d37ae6c0cbc44"
+
     def __init__(self, url):
         pass    
     pass
 
-def checkDnsMagic():
-    responses = dns.resolver.query("magic.moukaeritai.work", "TXT")
-    if len(responses) != 0:
-        logging.warning("Failed to get TXT record of magic.moukaeritai.work.")
-    if responses[0] != Marker.magic:
-        logging.warning("Magic number does not match with magic.moukaeritai.work.")
-
-if __name__ == "__main__":
-    checkDnsMagic()    
