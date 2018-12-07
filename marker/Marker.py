7a1b648601d11c68f7e76c1ca0b6e27fa5635145
Takashi SASAKI <takashi316@gmail.com>
2018-12-07 16:22:00 +0900

    add Test_Marker.py

--- marker/Marker.py
+++ marker/Marker.py
@@ -1,12 +1,16 @@
-import urllib.parse
-import dns.resolver
-import logging
-
-
-
 class Marker():
+    __slots__ = ["randomHex", "selfUrl", "targetUrl", "labelSet", 
+     "randomHexs", "selfUrls", "targetUrls", "labelSets",
+     "relativeUrls"]
 
-    def __init__(self, url):
-        pass    
-    pass
+    def __init__(self):
+        self.randomHex = ""
+        self.randomHexs = {}
+        self.selfUrl = ""
+        self.selfUrls = {}
+        self.targetUrl = ""
+        self.targetUrls = {}
+        self.labelSet = set()
+        self.labelSets = {}
+        self.relativeUrls = {}
 
---
+++ test/Test_Marker.py
@@ -0,0 +1,8 @@
+import marker.Marker
+import unittest
+
+class Test_Marker(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test(self):
+        m = marker.Marker.Marker()
